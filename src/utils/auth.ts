16893be6b15004c6ecad09866f2617254a5afcfc
Avocado <andrii.otroshenko@gmail.com>
2020-11-30 17:20:37 +0200

    fix: login bug

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -7,34 +7,38 @@ export const authUser: GetServerSideProps = async (
   context: GetServerSidePropsContext
 ): Promise<GetServerSidePropsResult<any>> => {
   const { jwt_token } = nextCookie(context)
-
-  if (context.req.url?.split('?')[0] === '/login') {
-    if (context.req && jwt_token) {
+  if (context.resolvedUrl.split('?')[0] === '/login') {
+    if (jwt_token) {
       context.res.writeHead(302, { Location: '/dashboard' })
       context.res.end()
     }
   } else {
-    if (context.req && !jwt_token) {
+    if (!jwt_token) {
       context.res.writeHead(302, { Location: '/login' })
       context.res.end()
     }
   }
 
-  const url = `${process.env.NEXT_PUBLIC_API}/api/user`
-  try {
-    const response = await fetch(url, {
-      mode: 'cors',
-      credentials: 'include',
-      headers: {
-        Authorization: `bearer ${jwt_token}`,
-      },
-    })
-    const jsonResponse = await response.json()
-    return {
-      props: { jsonResponse },
+  if (jwt_token) {
+    const url = `${process.env.NEXT_PUBLIC_API}api/user`
+    try {
+      const response = await fetch(url, {
+        headers: {
+          Authorization: `Bearer ${jwt_token}`,
+        },
+      })
+      const jsonResponse = await response.json()
+      return {
+        props: { jsonResponse },
+      }
+    } catch (e) {
+      console.error(e.message)
     }
-  } catch (e) {
-    console.error(e.message)
+  } else {
+    return {
+      props: {},
+    }
   }
+
   return { props: { message: 'undefined token' } }
 }
